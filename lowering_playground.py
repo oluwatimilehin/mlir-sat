34851fe9ae37ab20c0726ccc98106677262efeb5
Timi Adeniran <oluwatimilehinadeniran@gmail.com>
2025-01-06 09:46:10 +0100

    Execute linalg conversion

--- lowering_playground.py
+++ lowering_playground.py
@@ -1,4 +1,5 @@
 from io import StringIO
+from math import prod
 
 from xdsl.context import MLContext
 from xdsl.dialects import (
@@ -19,7 +20,15 @@ from xdsl.dialects import (
 from xdsl.dialects.builtin import Builtin
 from xdsl.interpreter import Interpreter, OpCounter
 from xdsl.interpreters import register_implementations
+from xdsl.interpreters.riscv_cf import RiscvCfFunctions
+from xdsl.interpreters.riscv_debug import RiscvDebugFunctions
+from xdsl.interpreters.riscv_func import RiscvFuncFunctions
+from xdsl.interpreters.riscv_libc import RiscvLibcFunctions
+from xdsl.interpreters.riscv_scf import RiscvScfFunctions
+from xdsl.interpreters.scf import ScfFunctions
 from xdsl.interpreters.shaped_array import ShapedArray
+from xdsl.interpreters.utils.ptr import TypedPtr
+
 from xdsl.parser import Parser as IRParser
 from xdsl.passes import PipelinePass
 from xdsl.printer import Printer
@@ -36,7 +45,6 @@ from xdsl.backend.riscv.lowering import (
 
 from xdsl.transforms import (
     canonicalize,
-    convert_linalg_to_loops,
     dead_code_elimination,
     lower_affine,
     lower_riscv_func,
@@ -46,10 +54,6 @@ from xdsl.transforms import (
 )
 
 
-def emulate_riscv(program: str):
-    from xdsl.interpreters.riscv_emulator import run_riscv
-
-    run_riscv(program, unlimited_regs=False, verbosity=3)
 
 
 def context() -> MLContext:
@@ -124,11 +128,28 @@ if __name__ == "__main__":
 
         print(f"Value: {io.getvalue()}")
 
+        shape = (2,2)
+
+        a_len = prod(shape)
+        b_len = prod(shape)
+        c_len = prod(shape)
+
+        a_shaped = ShapedArray(TypedPtr.new_float64([i + 1 for i in range(a_len)]), shape)
+        b_shaped = ShapedArray(TypedPtr.new_float64([(i + 1) / 100 for i in range(b_len)]), shape)
+        # riscv_c_shaped = ShapedArray(TypedPtr.new_float64([0.0] * c_len), shape)
+        c_shaped = ShapedArray(TypedPtr.new_float64([(i + 4) / 100 for i in range(b_len)] * c_len), shape)
+
+        print(f"A: {a_shaped}")
+        print(f"B: {b_shaped}")
+        print(f"C: {c_shaped}")
+
         riscv_op_counter = OpCounter()
         riscv_interpreter = Interpreter(module_op, listener=riscv_op_counter)
 
         register_implementations(
             riscv_interpreter, context(), include_wgpu=False, include_onnx=False
         )
+        riscv_interpreter.register_implementations(RiscvCfFunctions())
+        riscv_interpreter.call_op("distribute", (a_shaped.data_ptr.raw, b_shaped.data_ptr.raw, c_shaped.data_ptr.raw))
 
-        riscv_interpreter.call_op("main", ())
+        print(f"Result: {c_shaped}")
